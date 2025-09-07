35c0dbf25dc97acc9bfec537a520b2a1936eba79
bongholee <leebongho9204@gmail.com>
2025-09-07 21:02:50 +0900

    Jpa Entity에 동등성 비교 정책을 적용한 메서드 오버라이딩

--- src/main/kotlin/tobyspring/splearn/domain/BaseEntity.kt
+++ src/main/kotlin/tobyspring/splearn/domain/BaseEntity.kt
@@ -4,12 +4,29 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.proxy.HibernateProxy
 
 
 @MappedSuperclass
 abstract class BaseEntity(
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
-)
+) {
+    final override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        val oEffectiveClass =
+            if (other is HibernateProxy) other.hibernateLazyInitializer.persistentClass else other.javaClass
+        val thisEffectiveClass =
+            if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass else this.javaClass
+        if (thisEffectiveClass != oEffectiveClass) return false
+        other as BaseEntity
+
+        return id != null && id == other.id
+    }
+
+    final override fun hashCode(): Int =
+        if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass.hashCode() else javaClass.hashCode()
+
+}
